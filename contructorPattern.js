3f1b080769b4717684d4126d0dbc7f754ba096f6
Calin Ciupei <calin.ciupei@in2itive.ro>
2016-05-18 06:36:46 +0300

    Add example for basic constructor pattern

--- contructorPattern.js
+++ contructorPattern.js
@@ -1,11 +1,24 @@
 /**
  * Created by calin on 17-May-16.
+ *
+ * Simple version of a constructor pattern.
+ * Disatvantages for this pattern are:
+ * 1. Makes inheritance difficult.
+ * 2. Function as toString() are redefined for easch new object created using Mountain constructor
  */
 
 function Mountain(name, height, country){
     this.name = name;
     this.height = height;
     this.country = country;
+
+    this.toString = function () {
+        return "Mountain "+this.name+" has height "+this.height+" in country "+this.country;
+    }
 }
 
-var moldoveanu = new Mountain('Everst', 2600, "Ro");
+var annapurna = new Mountain('Annapurna', 8000, "Nepal");
+var makalu = new Mountain('Makalu', 27838, "Nepal");
+
+console.log(annapurna.toString());
+console.log(makalu.toString());
