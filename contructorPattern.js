cb99600cefb1b6bb58277c91d1e2b9d10d683cb9
Calin Ciupei <calin.ciupei@in2itive.ro>
2016-05-19 06:22:04 +0300

    Constructor with prototypes

--- contructorPattern.js
+++ contructorPattern.js
@@ -22,3 +22,25 @@ var makalu = new Mountain('Makalu', 27838, "Nepal");
 
 console.log(annapurna.toString());
 console.log(makalu.toString());
+
+
+/****
+* Constructor with prototypes
+* If a javscript constructor is called for creating an object, the all the properties of the constructor's
+* prototype are available to the new object.
+* */
+
+function Car(model, year, miles){
+    this.model = model;
+    this.year = year;
+    this. miles = miles;
+}
+
+/*
+ * syntax is Object.prototype.newMethod.
+ * it is used this way to avoid redefining the property object.
+*/
+Car.prototype.toString = function(){return this.model+" has done "+this.miles+"miles"};
+
+var civic = new Car("H Civic", 2990, 2001222);
+var mondeo = new Car("F Mondeo", 788, 5554788);
